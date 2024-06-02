4350269a932d1a0953997794005e659492b6ef92
FatihEmreOzer <av.femreozer@gmail.com>
2024-06-02 13:52:04 -0500

    -last

--- src/test/java/techrpoed/day07_MavenJUnit/C01_ClassWork.java
+++ src/test/java/techrpoed/day07_MavenJUnit/C01_ClassWork.java
@@ -34,20 +34,32 @@ public class C01_ClassWork {
             //5.Sign in buttonuna tiklayin
             WebElement signIn = driver.findElement(By.xpath("//*[@value= 'Sign in']"));
             signIn.click();
-            Thread.sleep(2000);
+            Thread.sleep(1000);
             driver.navigate().back();
 
             //6.Online Banking altinda Pay Bills sayfasina gidin
-
-
+            driver.findElement(By.xpath("(//*[text() =.])[11]")).click();
+            //--->//*[text() =. ][11] Bu sekilde text ile aldigimiz bir xpathde text degisse bile biz o webelementi handle edebiliriz
+            driver.findElement(By.xpath("//*[@id= 'pay_bills_link']")).click();
             //7.amount kisminda yatirmak istediginiz herhangi bir miktar yazin
+            driver.findElement(By.xpath("//*[@id = 'sp_amount']")).sendKeys("1250");
+            Thread.sleep(1000);
 
             //8.tarih kismina "2020-09-10" yazdirin
+            driver.findElement(By.xpath("//*[@id = 'sp_date']")).sendKeys("2020-09-10");
+            Thread.sleep(1000);
 
             //9.pay buttonuna tiklayin
+            driver.findElement(By.id("pay_saved_payees")).click();
 
             //10."The payment was succesfully submitted." mesajinin ciktigini kontrol edin
-
-        }
+            WebElement sonucYazisi = driver.findElement(By.xpath("(//span)[1]"));
+            System.out.println(sonucYazisi.getText());
+            if (sonucYazisi.getText().equals("The payment was successfully submitted.")) {
+                    System.out.println("TEST PASSED");
+            }else System.out.println("TEST FAILED");
+            Thread.sleep(1000);
+            driver.close();
+    }
     }
 
