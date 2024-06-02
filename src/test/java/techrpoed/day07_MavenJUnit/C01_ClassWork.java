d449b74537b980309b91033f5c05f008e7e0f601
FatihEmreOzer <av.femreozer@gmail.com>
2024-06-02 13:00:00 -0500

    -last

---
+++ src/test/java/techrpoed/day07_MavenJUnit/C01_ClassWork.java
@@ -0,0 +1,53 @@
+package techrpoed.day07_MavenJUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_ClassWork {
+    public static void main(String[] args) throws InterruptedException {
+
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+            //1. http://zero.webappsecurity.com sayfasina gidin
+            driver.get("http://zero.webappsecurity.com");
+
+            //2.Signin buttonuna tiklayin
+            driver.findElement(By.id("signin_button")).click();
+
+            //3.Login alanine "usarname" yazdirin
+            WebElement login = driver.findElement(By.xpath("//*[@id= 'user_login']"));
+            login.sendKeys("username");
+
+            //4.Password alanina "password" yazdirin
+            WebElement password = driver.findElement(By.xpath("//*[@id= 'user_password']"));
+            password.sendKeys("password");
+
+            //5.Sign in buttonuna tiklayin
+            WebElement signIn = driver.findElement(By.xpath("//*[@value= 'Sign in']"));
+            signIn.click();
+            Thread.sleep(2000);
+            driver.navigate().back();
+
+            //6.Online Banking altinda Pay Bills sayfasina gidin
+
+
+            //7.amount kisminda yatirmak istediginiz herhangi bir miktar yazin
+
+            //8.tarih kismina "2020-09-10" yazdirin
+
+            //9.pay buttonuna tiklayin
+
+            //10."The payment was succesfully submitted." mesajinin ciktigini kontrol edin
+
+        }
+    }
+
