b666f19201ba6869baf49510b070766bb54cb1ce
FatihEmreOzer <av.femreozer@gmail.com>
2024-06-25 20:39:28 -0500

    -last

--- src/test/java/techrpoed/day07_MavenJUnit/C02_BeforeAfter.java
+++ src/test/java/techrpoed/day07_MavenJUnit/C02_BeforeAfter.java
@@ -2,6 +2,7 @@ package techrpoed.day07_MavenJUnit;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class C02_BeforeAfter {
     @After
@@ -12,9 +13,24 @@ public class C02_BeforeAfter {
 
     @Before
     public void setUp(){
-
+        System.out.println("Her Test Methodundan once bir kez calisir");
 
 
     }
+
+    @Test
+    public  void test1(){
+        System.out.println("Test1 methodunu calistir");
+    }
+    @Test
+    public  void test2(){
+        System.out.println("Test2 methodunu calistir");
+    }
+    @Test
+    public  void test3(){
+        System.out.println("Test3 methodunu calistir");
+    }
+
+
 
 }
