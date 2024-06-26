bf692df04ff11ed17735b5b263af3279afe15762
FatihEmreOzer <av.femreozer@gmail.com>
2024-06-25 20:36:10 -0500

    -last

--- src/test/java/techrpoed/day07_MavenJUnit/C02_BeforeAfter.java
+++ src/test/java/techrpoed/day07_MavenJUnit/C02_BeforeAfter.java
@@ -1,4 +1,20 @@
 package techrpoed.day07_MavenJUnit;
 
+import org.junit.After;
+import org.junit.Before;
+
 public class C02_BeforeAfter {
+    @After
+    public void tearDown(){
+        System.out.println("Her Test Methodundan sonra bir kez calisir");
+
+    }
+
+    @Before
+    public void setUp(){
+
+
+
+    }
+
 }
