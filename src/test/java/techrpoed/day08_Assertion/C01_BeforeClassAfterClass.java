d7e911162768ff6800cd65c1cce88e429492fc9a
FatihEmreOzer <av.femreozer@gmail.com>
2024-06-28 12:27:12 -0500

    -last

--- src/test/java/techrpoed/day08_Assertion/C01_BeforeClassAfterClass.java
+++ src/test/java/techrpoed/day08_Assertion/C01_BeforeClassAfterClass.java
@@ -1,4 +1,28 @@
 package techrpoed.day08_Assertion;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
 public class C01_BeforeClassAfterClass {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        System.out.println("Her classdan once BeforeClass methoodu bir kez calisir");
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        System.out.println("Her classdan sonra AfterClass methoodu bir kez calisir");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("Her methoddan once before methodu bir kez calisir");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("Her methoddan sonra after methodu bir kez calisir");
+    }
 }
