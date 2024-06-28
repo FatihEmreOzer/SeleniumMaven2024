d018e79754fc7efb60ddcaaeddb497cc2c2e24a2
FatihEmreOzer <av.femreozer@gmail.com>
2024-06-28 13:24:20 -0500

    -last

--- src/test/java/techrpoed/day08_Assertion/C03_BeforeClassAfterClass_Assertion.java
+++ src/test/java/techrpoed/day08_Assertion/C03_BeforeClassAfterClass_Assertion.java
@@ -1,10 +1,7 @@
 package techrpoed.day08_Assertion;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -17,9 +14,7 @@ public class C03_BeforeClassAfterClass_Assertion {
     @BeforeClass
     public static void beforeClass() throws Exception {
         WebDriverManager.chromedriver().setup();
-        driver= new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
 
     }
 
@@ -34,20 +29,20 @@ public class C03_BeforeClassAfterClass_Assertion {
 
     @Test
     public void test01() throws InterruptedException {
-        driver.findElement(By.xpath("//*[class='eicon-close']")).click();
+        //driver.findElement(By.xpath("//*[class='eicon-close']")).click();
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@type='search']")).sendKeys("developer", Keys.ENTER);
+        driver.findElement(By.xpath("//*[@type='text']")).sendKeys("qa", Keys.ENTER);
     }
 
     @Test
     public void test02() throws InterruptedException {
-        driver.findElement(By.xpath("//*[class='eicon-close']")).click();
+       // driver.findElement(By.xpath("//*[class='eicon-close']")).click();
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@type='search']")).sendKeys("developer", Keys.ENTER);
+        driver.findElement(By.xpath("//*[@type='text']")).sendKeys("developer", Keys.ENTER);
     }
 
-    @AfterClass
-    public static void afterClass() throws Exception {
+    @After
+    public void tearDown() throws Exception {
         driver.close();
     }
 }
