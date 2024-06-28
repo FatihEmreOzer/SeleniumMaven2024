1878cb0a82f109927e44fdd1b6e1ea0616f3b7f1
FatihEmreOzer <av.femreozer@gmail.com>
2024-06-28 12:40:51 -0500

    -last

--- src/test/java/techrpoed/day08_Assertion/C02_BeforeClassAfterClass.java
+++ src/test/java/techrpoed/day08_Assertion/C02_BeforeClassAfterClass.java
@@ -1,5 +1,31 @@
 package techrpoed.day08_Assertion;
 
-public class C02_BeforeClassAfterClass {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
+public class C02_BeforeClassAfterClass {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1500));
+        driver.get("https://techproeducation.com");
+    }
+
+    @Test
+    public void test01() {
+
+    }
 }
