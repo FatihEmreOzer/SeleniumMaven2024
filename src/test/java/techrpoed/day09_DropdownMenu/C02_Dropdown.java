2b211a65a386756d045c1772e7269d2fdd75f939
FatihEmreOzer <av.femreozer@gmail.com>
2024-06-30 11:40:53 -0500

    -last

---
+++ src/test/java/techrpoed/day09_DropdownMenu/C02_Dropdown.java
@@ -0,0 +1,22 @@
+package techrpoed.day09_DropdownMenu;
+
+public class C02_Dropdown {
+    /*
+    DROPDOWN---> Altbasliklarin oldugu acilir menu listesidir
+    Dropdown'u handle etmek icin
+    1. Dropdown menu ilk olarak locate edilir
+    2-Select objesi olusturulur
+    3-Select objesinin ddm webeleementinin icerigine ve seceneklerine erisim saglamasi icin SELECT sinifina arguman olarak
+    locate ettigimiz webelementi koyariz
+
+    SYNTAX:
+    Select select = new Select(ddm webelementi)
+
+    4- Select class'i sadece <select> tagi ile olusturulmus dropdown menulere uygulanabilir
+    5- Select objesi ddm'yu handle edebilmek icin 3 method kullaniri
+        - selectByVisibleText() -->DDm deki elemente gorunur metin ile ulasmak icin kullanilir.
+        - selectByIndex() --> index ile ulasmak icin kullanilir
+        - selectByValue() --> elementin degeri ila ulasmak icin kullanilir
+     */
+
+}
